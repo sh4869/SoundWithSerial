5a75ed90468d323eb4d13734bb6a6166739c3b61
sh4869 <nobuk4869@gmail.com>
2014-08-05 02:02:21 +0900

    とりあえずここまで

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            SerialPort sepo;
+            sepo.BaudRate = 9600;
+            sepo.PortName = "";
+            sepo.Open();
+
             System.Media.SystemSounds.Beep.Play();
 
         }
